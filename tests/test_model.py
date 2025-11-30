5c4fd2f686741132788320c2793c6a371831c648
Andreas Motl <andreas.motl@crate.io>
2025-11-30 18:44:56 +0100

    History mode: Minimal version of `WriteHistoryBatch` gRPC method

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,7 @@
+import re
+
+import pytest
+
 from cratedb_fivetran_destination.model import SqlBag
 
 
@@ -5,3 +9,9 @@ def test_sqlbag(engine):
     bag = SqlBag().add("SELECT 23").add("SELECT 42")
     with engine.connect() as connection:
         bag.execute(connection)
+
+
+def test_sqlbag_add_none():
+    with pytest.raises(TypeError) as excinfo:
+        SqlBag().add(None)
+    excinfo.match(re.escape("Input SQL must be str or SqlBag, not <class 'NoneType'>"))
