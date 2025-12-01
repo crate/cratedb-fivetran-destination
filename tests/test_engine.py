90e818443fb4edb4dbb78392806960397c86bf5e
Andreas Motl <andreas.motl@crate.io>
2025-12-01 01:59:47 +0100

    Schema migrations: History table pre-flight checks for emptiness

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,7 +1,32 @@
+import typing as t
+
+import pytest
+import sqlalchemy as sa
+
 from cratedb_fivetran_destination.engine import EarliestStartHistoryStatements
-from cratedb_fivetran_destination.model import TableInfo
+from cratedb_fivetran_destination.model import FivetranTable, TableInfo
+from cratedb_fivetran_destination.schema_migration_helper import SchemaMigrationHelper
 
 
 def test_earliest_start_history_statements():
-    eshs = EarliestStartHistoryStatements(table=TableInfo("foo.bar"), records=[])
+    eshs = EarliestStartHistoryStatements(table=TableInfo("testdrive.foo"), records=[])
     assert eshs.hard_delete_with_timestamp() is None
+
+
+def test_schema_migration_helper_validate_history_table(engine):
+    # FIXME: Remove `table_map`. This is part of the simulation.
+    table_map: t.Dict[str, FivetranTable] = {}
+    smh = SchemaMigrationHelper(engine, table_map)
+
+    # Create an empty table to trigger the error condition.
+    with engine.connect() as conn:
+        conn.execute(sa.text("CREATE TABLE testdrive.foo (id INT);"))
+
+    # Run history table operation pre-flight checks.
+    with pytest.raises(ValueError) as excinfo:
+        smh._validate_history_table(
+            schema="testdrive", table="foo", operation_timestamp="2005-05-23T20:57:00Z"
+        )
+
+    # Validate the assertion.
+    assert excinfo.match("table is empty")
